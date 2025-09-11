ed3fe8a66d22ecc6002c791b9f05029719dce8b9
Taisuke Maekawa <fygar256@outlook.jp>
2025-09-12 05:08:44 +0900

    Add files via upload

---
+++ truerand.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+# truerand.py
+# pythonで、/dev/randomからの真性乱数を利用して乱数を発生するモジュール
+#
+import os
+import binascii
+
+# n byteのRAND_MAXを返す関数
+# in : n バイト長
+# out: nバイトのRAND_MAX
+def rand_max(n):
+    v=0
+    for i in range (0,n):
+        v=v*256+0xff
+    return(v)
+
+# n byte の真性乱数を返す関数
+# parameter : n バイト長
+# return    :   n バイトの正の整数の乱数
+def rand(n):
+    f=open("/dev/random",'rb') # /dev/randomを開く
+    randomdata=f.read(n) # nバイト読み出し
+    f.close() # /dev/randomを閉じる
+    randomhex=binascii.hexlify(randomdata) #１６進の文字列に変換
+    randomint=int(randomhex,16) # 整数に変換
+    return(randomint)
+
+#
+# n byte の精度の[0,1)の実数乱数を返す関数
+# parameter : n バイト長
+# return    : [0,1)の乱数
+#
+# 注:割り算を使っているので本当の真性乱数ではない
+#
+def rand_f(n):
+    return(rand(n)/rand_max(n))
+
+#
+# 0〜k-1の乱数を返す関数
+# parameters : n バイト長
+#              k 整数
+# return     : 0〜(k-1)の整数乱数
+#
+# 注:剰余演算を使っているので本当の真性乱数ではない
+#
+def randomint(k,n):
+    return(rand(n)%k)
+
+if __name__=="__main__":
+    b=8
+    times=20
+    print(f"{b*8}bit正の真性整数乱数")
+    for _ in range(times):
+        print(rand(b),end=" ")
+
+    print("")
+    print("実数[0,1)の範囲の実数乱数")
+    for _ in range(times):
+        print(rand_f(b),end=" ")
+    print("")
+    print("整数0〜99の乱数")
+    for _ in range(times):
+        print(randomint(100,b),end=" ")
+    print("")
+
